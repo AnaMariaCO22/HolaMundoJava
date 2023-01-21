6389b937887ab76920a22c0ad8d96d4401de4656
Ana Velasquez <anamariavego00@gmail.com>
2023-01-21 13:34:33 -0500

    Este es el primer commit de mi proyecto con Java

---
+++ src/Variables.java
@@ -0,0 +1,10 @@
+public class Variables {
+    public static void main(String[] args) {
+        //Declarando una variable
+        int speed = 10;
+        System.out.println(speed);
+        int salary=100;
+        String employeeName = "Anahí Salgado";
+        System.out.println(employeeName);
+    }
+}
