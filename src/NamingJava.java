529e0bc4b715429ca97febb03e00d00578745a9e
Ana Velasquez <anamariavego00@gmail.com>
2023-01-21 15:18:12 -0500

    Agregado c{odigo de convensi{on de nimbres y camel case

---
+++ src/NamingJava.java
@@ -0,0 +1,19 @@
+public class NamingJava {
+    public static void main(String[] args) {
+        int celphone=333333487;
+        int Celphone=33763487;
+        System.out.println(Celphone);
+
+        String $money="CPL";
+        String _junda="Juanda te quiero";
+        String Juanda_lol ="Eres divertido";
+
+        int POSITION =-5;
+        int MAX_WIDTH = 9999;
+        int MIN_WIDTH = 1;
+
+        //Lowwer Camel Case
+        String fullName = "Ana Maria Velasquez Gonzalez";
+        int sizeInCentimeters = 26;
+    }
+}
