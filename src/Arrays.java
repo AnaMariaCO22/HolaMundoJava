88ddd2e0a5117208679f7f3a5e6bcf86e7f979c2
Ana Velasquez <anamariavego00@gmail.com>
2023-01-31 23:33:43 -0500

    Commit de clases de for anidados para recorrer arreglos, fori y foreach, fin del curso, wowww

--- src/Arrays.java
+++ src/Arrays.java
@@ -8,6 +8,62 @@ public class Arrays {
         int [][][] numbers = new int[2][2][2];
         int [][][][] numbers4=new  int[2][2][2][2];
         
+        androidVersion[0] = "Apple Pie";
+        androidVersion[1] = "Banana Bread";
+        androidVersion[2] = "Cupcake";
+        androidVersion[3] = "Donut";
+/*
+        for (int i = 0; i < androidVersion.length ; i++) {
+            System.out.println(androidVersion[i]);
+
+        }*/
+        for (String android:androidVersion) {
+            System.out.println(android);
+        }
+        /*
+        System.out.println(androidVersion[0]);
+        System.out.println(androidVersion[1]);
+        System.out.println(androidVersion[2]);
+        System.out.println(androidVersion[3]);
+        */
+
+        cities[0][0]="Colombia";
+        cities[0][1]="Medellin";
+        cities[1][0]="Colombia";
+        cities[1][1]="Bogotá";
+        cities[2][0]="Mexico";
+        cities[2][1]="Guadalajara";
+        cities[3][0]="Mexico";
+        cities[3][1]="CDMX";
+
+        for (int i = 0; i < cities.length ; i++) {
+            for (int j = 0; j < cities[i].length; j++) {
+                System.out.println(cities[i][j]);
+            }
+        }
+        for (String[] pair:cities) {
+            for (String name:pair) {
+                System.out.println(name);
+            }
+        }
+/*
+
+        System.out.println(cities[0][0]);
+        System.out.println(cities[0][1]);
+        System.out.println(cities[1][0]);
+        System.out.println(cities[1][1]);
+        System.out.println(cities[2][0]);
+        System.out.println(cities[2][1]);
+        System.out.println(cities[3][0]);
+        System.out.println(cities[3][1]);
+*/
+        String [][][][] animals = new String[2][3][2][2];
+        animals[1][0][0][1]="Monkey";
+
+        System.out.println(animals[1][0][0][1]);
 
     }
 }
+
+
+
